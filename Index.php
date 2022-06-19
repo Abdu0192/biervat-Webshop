8b4ae7cafda502b842dcaba6e1094f359aa245fe
Abdu0192 <82876968+Abdu0192@users.noreply.github.com>
2022-06-19 05:59:34 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>                                                            
+<html lang="nl">  
+                                                         
+<head>                                                                  
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="main.js">
+  
+    
+        <title></title>
+</head>                                                                     
+<body class="">                                                                      
+<div class="logo">
+     <a href="#"><img src="goedkoopdrank_logo_klein.png"></a>
+</div>
+<nav>
+ <ul>
+<li><a href="#"></a></a></li>
+    <li><a href="#">Home</a></a></li>
+    <li><a href="#">Producten</a></a></li>
+    <li><a href="#">Contact</a></a></li>
+ </ul>
+
+</nav>
+
+<main class = grid>
+    
+ <article>           
+          <div class="text">
+            <a href="" ><img src="heineken_biervat_50_liter_goedkoop_prijs-228x228.jpg"></a>
+            <h3><a href="">Heineken Bier Fust Vat 50 Liters</a></h3>
+          <h3>Prijs  $114,90          </h3>
+          <form action="/action_page.php">
+            <input type="file" id="myFile" name="filename">
+  
+          </form>
+          <h3></h3>
+         <button><a href=""> add to cart</a></button> 
+        </div>
+       
+       </article>
+
+    
+
+</main>
+
+
+
+</body>                                                                  
+
+  
+<div class="wrapper">
+  <img src="img/cookie.png" alt="">
+  <div class="content">
+      <header>18+</header>
+      <p>Om ons Website to gebruiken moet je boven 18+ zijn.</p>
+      <div class="buttons">
+          <button class="item">ja</button>
+          <a href="#" class="item">Nee</a>
+      </div>
+  </div>
+</div>
+<script src="js/main.js"></script>
+
+
+
+
+
+</html>
